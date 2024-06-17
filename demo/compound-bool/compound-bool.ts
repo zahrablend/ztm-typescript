cc7ec03af8ed68bf9a55bc11c170135da1f341e7
Zahra Zvirzdinaite <zahra_zvirzdin@outlook.com>
2024-06-17 09:50:45 +0300

    Add arithmetics and compound-bool

--- demo/compound-bool/compound-bool.ts
+++ demo/compound-bool/compound-bool.ts
@@ -1,3 +1,28 @@
 /* eslint-disable */
 import { strict as assert } from "assert";
 
+// NOT
+const writing = true;
+const reading = !writing;
+assert.equal(reading, false);
+
+// OR
+// (pick 1 movie)
+const rating = 9;
+const favoriteMovie = false;
+const suggestMovie = rating > 8 ||  favoriteMovie;
+assert.equal(suggestMovie, true);
+
+// AND
+const age = 18;
+const isTeenager = age >= 13 && age < 20;
+assert.equal(isTeenager, true);
+
+//our package
+const packageWeight = 30;
+const packageLength = 50;
+const shippingDistance = 99;
+const air = true;
+const priority = true;
+const feeExemption = false;
+const extraFee = !feeExemption && (packageWeight > 25 || packageLength > 40);
