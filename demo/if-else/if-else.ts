92d3b0ff8ad798daaef0dae1a6dc6f5c2396a334
Zahra Zvirzdinaite <zahra_zvirzdin@outlook.com>
2024-06-18 10:28:11 +0300

    Add if-else

--- demo/if-else/if-else.ts
+++ demo/if-else/if-else.ts
@@ -9,3 +9,64 @@ import { strict as assert } from "assert";
 //
 // Useful links:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else
+
+const answer = 2 + 2;
+assert.equal(answer, 4);
+
+if (answer > 4) {
+    console.log("> 4");
+}
+
+if (answer < 4) {
+    console.log("<4");
+} else if (answer > 4) {
+    console.log(">4");
+} else {
+    console.log("=4");
+}
+
+const age = 6;
+const likesHavingFun = true;
+
+if (age <=12 && likesHavingFun) {
+    // play in the part
+} else if (age > 12 && !likesHavingFun) {
+    //be bored
+} else {
+    // work on hobbies
+}
+
+const hasTheSkills = true;
+const day = "Tuesday";
+const hoursWorked = 9;
+const totalOvertime = 0.5;
+const holidaySeason = false;
+
+function approveMoreWork() {
+    if (!hasTheSkills) {
+        // go home
+        return;
+    }
+
+    const canWorkOvertime = hoursWorked > 8 && totalOvertime < 1;
+    if (!hasOvertimeHours(hoursWorked, totalOvertime)) {
+        // go home
+        return;
+    }
+
+    if (!isBusyDay(day, holidaySeason)) {
+        // go home
+        return;
+    }
+
+    // approve work
+}
+
+function isBusyDay(day: string, holidaySeason: boolean): boolean {
+    return holidaySeason || day === "tuesday";
+}
+
+function hasOvertimeHours(hoursWorked: number, totalOvertime: number): boolean {
+    const hasHours = hoursWorked > 8 && totalOvertime < 1;
+    return hasHours;
+}
