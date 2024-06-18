4f917cb87837581de67c733e2ffba7c17ffd23cf
Zahra Zvirzdinaite <zahra_zvirzdin@outlook.com>
2024-06-18 11:09:02 +0300

    Add switch and respective tasks

--- demo/switch/switch.ts
+++ demo/switch/switch.ts
@@ -3,3 +3,16 @@ import { strict as assert } from "assert";
 
 // Useful links:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/switch
+
+let someNumber = parseInt("3");
+const sample = "sample";
+const someString = `${sample}`;
+
+switch (someNumber) {
+    case 1:
+        break;
+    case 2:
+        break;
+    default:
+        // some code
+}
