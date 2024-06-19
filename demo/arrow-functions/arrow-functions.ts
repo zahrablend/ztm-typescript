99b354fe0344d4de3b9e3800497c9eb5ee569d4e
Zahra Zvirzdinaite <zahra_zvirzdin@outlook.com>
2024-06-19 10:40:32 +0300

    Add arrow-functions

--- demo/arrow-functions/arrow-functions.ts
+++ demo/arrow-functions/arrow-functions.ts
@@ -9,3 +9,37 @@
 // 
 // Useful links:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+
+const sum = function (lhs: number, rhs: number): number {
+    return lhs + rhs;
+}
+const two = sum(1, 1);
+
+const arrowSum = (lhs: number, rhs: number): number => {
+    return lhs + rhs;
+}
+
+const arrowSub = (lhs: number, rhs: number): number => {
+    return lhs - rhs;
+}
+
+const ten = arrowSum(5, 5);
+
+function calculate(fn: (lhs: number, rhs: number) => number) {
+    const result = fn(5, 6);
+    console.log(result);
+}
+
+calculate(arrowSum);
+calculate(arrowSub);
+
+type calculationFn = (lhs: number, rhs: number) => number;
+function calculate2(fn: calculationFn, lhs: number, rhs: number):  number {
+    const result = fn(rhs, lhs);
+    if (result === 7 ) {
+        console.log("you win extra!");
+    }
+    return result;
+}
+
+console.log(calculate2(arrowSum, 4, 5));
