de3a5745b412af983d675c4361208f82ef62c4af
Zahra Zvirzdinaite <zahra_zvirzdin@outlook.com>
2024-06-20 11:20:55 +0300

    Add tuples

--- demo/tuples/tuples.ts
+++ demo/tuples/tuples.ts
@@ -10,3 +10,34 @@ import { strict as assert } from "assert";
 //
 // Useful links:
 // https://www.typescriptlang.org/docs/handbook/2/objects.html#tuple-types
+
+type Title = string;
+type PublishYear = number;
+// tuple
+type Book = [Title, PublishYear];
+
+const sample: Book = ["sample", 1980];
+assert.equal(sample[0], "sample");
+assert.equal(sample[1], 1980);
+
+// return tuple from a function
+function coord(): [number, number] {
+    return [3, 5];
+}
+
+const coordinate = coord();
+assert.equal(coordinate[0], 3);
+assert.equal(coordinate[1], 5);
+
+// destructure (split) tuple
+const [x, y] = coord();
+assert.equal(x, 3);
+assert.equal(y, 5);
+
+// put a tuple inside an array
+const multi: [number, number][] = [
+    coord(),
+    [1, 1],
+    [0, 0],
+    [9, 9],
+];
