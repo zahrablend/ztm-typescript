bb68c5c9b3dee6060aa27bb9b70409b35caac9c3
Zahra Zvirzdinaite <zahra_zvirzdin@outlook.com>
2024-06-20 10:55:58 +0300

    Add array-objects and exercises

--- demo/array-object/array-object.ts
+++ demo/array-object/array-object.ts
@@ -1,2 +1,24 @@
 /* eslint-disable */
 import { strict as assert } from "assert";
+
+type Link = {
+    title: string;
+    url: string;
+};
+
+const microsoft = {
+    title: "microsoft",
+    url: "microsoft.com",
+};
+
+const typescript = {
+    title: "TypeScript",
+    url: "typescriptlang.org",
+};
+
+const links = [microsoft, typescript];
+// access url property of typescript array
+const tsUrl = links[1].url;
+
+// edit a property
+links[0].title = "Microsoft";
