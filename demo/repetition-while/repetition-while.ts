7f7c7af512edb56586de87c416776525bd0dddf3
Zahra Zvirzdinaite <zahra_zvirzdin@outlook.com>
2024-06-27 11:29:07 +0300

    Add while loop and exercise

--- demo/repetition-while/repetition-while.ts
+++ demo/repetition-while/repetition-while.ts
@@ -5,3 +5,36 @@
 // 
 // Useful links:
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/while
+
+let i = 1;
+while (i <= 5) {
+    console.log(`${i}`);
+    i++;
+}
+
+i = 1;
+while (i <= 5) {
+    console.log(`${i}`);
+    break;
+}
+
+
+i = 1;
+while (i <= 5) {
+    if (i === 4) {
+        i++;
+        continue;
+    }  
+    console.log(`${i}`);
+    i++;
+}
+
+i = 1;
+while (true) {
+    if (i >= 30) {
+        console.log("out of infinite loop");
+        break;
+    }
+    i += 2;
+}
+
